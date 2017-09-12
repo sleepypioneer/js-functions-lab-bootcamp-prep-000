a161b10ff458ad6480167dba2d335e4195ec80bf
Jessica Greene <jessica0greene@gmail.com>
2017-09-12 13:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function holidayCountdown(holiday, days) {
   return "It\'s ${days} days until ${holiday}!";
 }
 
- happyHolidaysTo("you");
- happyHolidayTo("Independence Day", "you");
- holidayCountdown("Mother's Day", 20);
+happyHolidays();
+happyHolidaysTo("you");
+happyHolidayTo("Independence Day", "you");
+holidayCountdown("Mother's Day", 20);
