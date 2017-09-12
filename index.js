4a435602c9175b1e5ca9f4c623d692a356a9c759
Jessica Greene <jessica0greene@gmail.com>
2017-09-12 13:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,22 +3,19 @@ function happyHolidays() {
   return "Happy holidays!";
 };
 
-happyHolidays()
 
 function happyHolidaysTo(name) {
+  var name = 'you'
   return "Happy holidays, ${name}!";
 };
 
-happyHolidaysTo('you')
 
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!";
 };
 
-happyHolidayTo('Independence Day', 'you')
 
 function holidayCountdown(holiday, days) {
   return "It\'s ${days} days until ${holiday}!";
 };
 
-holidayCountdown("Mother's Day", 20)
