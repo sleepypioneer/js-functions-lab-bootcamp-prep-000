9a1e5dc72b28c2cb8c0f0c5fde93b1ef6ae060b0
Jessica Greene <jessica0greene@gmail.com>
2017-09-12 13:17:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-// write your code below!
+// write your code below
+
 happyHolidays() {
   return "Happy holidays!";
 }
-
-happyHolidays();
 
 happyHolidaysTo(name) {
   return 'Happy Holiday, ${name}!';
 }
 
-happyHolidaysTo('Jessica');
 
 happyHolidayTo(holiday, name) {
   return 'Happy ${holiday}, ${name}';
 }
 
-happyHolidayTo('Easter', 'Paul');
 
 holidayCountdown(holiday, days) {
   return 'It\'s ${days} days until ${holiday}!';
 }
 
-holidayCountdown('Christmas', 40);
+happyHolidays();
+
